bbb977c5ddc8ee99f099250d201747f87a73f154
Muhammet933321 <Muhammet963321@gmail.com>
2024-03-23 19:59:37 +0300

    Deneme

--- GorselProgramlama#01/Form1.cs
+++ GorselProgramlama#01/Form1.cs
@@ -6,5 +6,10 @@ namespace GorselProgramlama_01
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
